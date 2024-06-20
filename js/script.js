117881e94b361a73fda400a6efa5a6a07c166018
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-20 21:41:59 +0000

    made a web page

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const baseLength = parseInt(document.getElementById('base-length').value)
+  const baseWidth = parseInt(document.getElementById('base-width').value)
+  const heighOfPyramid = parseInt(document.getElementById('height').value)
+
+  // process
+  const volumeOfPyramid = (baseLength * baseWidth * heighOfPyramid) / 3
+
+  // output
+  document.getElementById("volume").innerHTML = 'Volume is: ' + volumeOfPyramid.toFixed(2) + ' m'
+}
